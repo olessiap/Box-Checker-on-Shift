68648add9a96f3a25f8bfba92e58085d888d7be8
Olessia Potapova <olessia.potapova@gmail.com>
2019-04-16 13:30:25 -0300

    middle boxes get checked when holding down shift

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('yes');
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+function handleCheck(e) {
+    //check if SHIFT was pressed
+    //AND that check is checked
+    let inBetween = false;
+    if(e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if(checkbox === this || checkbox === lastChecked ) {
+                inBetween = !inBetween;
+            }
+            if(inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', handleCheck);
+})
